b9384080e00a0455e40173b8ce130467c6bf9b9a
moqiq <moqiqin@live.com>
2025-12-28 13:20:53 +0800

    update

--- installer/pack.ps1
+++ installer/pack.ps1
@@ -4,4 +4,5 @@ Remove-Item DumbDrawPhD_env.tar.gz -Recurse -Force
 conda pack -n py39 -o DumbDrawPhD_env.tar.gz --ignore-missing-files
 Remove-Item  DumbDrawPhD_Setup.exe -Recurse -Force
 & "C:\Program Files (x86)\NSIS\makensis.exe" install.nsi
+Remove-Item DumbDrawPhD_env.tar.gz -Recurse -Force
 
