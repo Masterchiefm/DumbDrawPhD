92ceac4f2091685158e72924516d2ec4ac227355
moqiq <moqiqin@live.com>
2025-12-28 13:43:03 +0800

    update

--- installer/pack.ps1
+++ installer/pack.ps1
@@ -1,5 +1,9 @@
 conda activate py39
+cd ..
+pip uninstall DumbDrawPhD -y
+pip install .
 conda env list
+cd installer
 Remove-Item DumbDrawPhD_env.tar.gz -Recurse -Force
 conda pack -n py39 -o DumbDrawPhD_env.tar.gz --ignore-missing-files
 Remove-Item  DumbDrawPhD_Setup.exe -Recurse -Force
