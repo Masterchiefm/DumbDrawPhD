db3c1c9012cc98ee494bdf93bee4a267e33468ee
愿乘风归去 <moqiqin@live.com>
2025-12-29 13:59:58 +0800

    Update pack.ps1

--- installer/pack.ps1
+++ installer/pack.ps1
@@ -1,12 +1,12 @@
 conda activate py39
 cd ..
-pip uninstall DumbDrawPhD -y
+pip uninstall DumbyDraw -y
 pip install .
 conda env list
 cd installer
-Remove-Item DumbDrawPhD_env.tar.gz -Recurse -Force
-conda pack -n py39 -o DumbDrawPhD_env.tar.gz --ignore-missing-files
-Remove-Item  DumbDrawPhD_Setup.exe -Recurse -Force
+Remove-Item DumbyDraw_env.tar.gz -Recurse -Force
+conda pack -n py39 -o DumbyDraw_env.tar.gz --ignore-missing-files
+Remove-Item  DumbyDraw_Setup.exe -Recurse -Force
 & "C:\Program Files (x86)\NSIS\makensis.exe" install.nsi
-Remove-Item DumbDrawPhD_env.tar.gz -Recurse -Force
+Remove-Item DumbyDraw_env.tar.gz -Recurse -Force
 
